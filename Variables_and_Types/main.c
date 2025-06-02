cee2cc4009beb6ad1ebd6137c1719e055917c3df
hmz97-dev <bdhh1112@gmail.com>
2025-06-02 13:25:52 +0000

    variables-and-types

--- Variables_and_Types/main.c
+++ Variables_and_Types/main.c
@@ -6,8 +6,9 @@ int main() {
   double c = 5.25;
   float sum;
 
-  /* Your code goes here */
+  sum = a + b + c;
 
-  printf("The sum of a, b, and c is %f.", sum);
+  printf("The sum of a, b, and c is %f.\n", sum);
   return 0;
 }
+
